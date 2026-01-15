7cdf52272e0ffd2ce2c40c1cce58fa60ae8acfc1
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-01-15 13:45:46 +0530

    changed blink tab behavior

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,8 +3,6 @@ return {
     opts = {
         keymap = {
             preset = "super-tab",
-            ["<Tab>"] = { "select_and_accept" },
-            ["<S-Tab>"] = { "select_prev" },
             ["<C-k>"] = { "select_prev", "fallback" },
             ["<C-j>"] = { "select_next", "fallback" },
             ["<C-u>"] = { "scroll_documentation_up", "fallback" },

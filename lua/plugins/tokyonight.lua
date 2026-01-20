02013b214c9529e156157886b220e3cbe2b0c9fe
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-01-20 13:04:54 +0530

    theme changes

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        styles = {
+            comments = { italic = true },
+            keywords = { italic = true },
+            functions = {},
+            variables = {},
+        },
+    },
+}

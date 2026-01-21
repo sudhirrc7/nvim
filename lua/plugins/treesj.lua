bb254e928365937e356662905b403627de21efcc
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-01-21 12:39:55 +0530

    fixed treesj

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -2,7 +2,4 @@ return {
     "Wansmer/treesj",
     -- keys = { '<space>m', '<space>j', '<space>s' },
     dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
-    config = function()
-        require("treesj").setup({})
-    end,
 }

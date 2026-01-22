a1a1ea50e2758591fe7efa85d82e8e517c4e0a6b
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-01-22 19:11:24 +0530

    smallchange

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
             "LazyVim/LazyVim",
             import = "lazyvim.plugins",
             opts = {
-                colorscheme = "onedark_vivid",
+                colorscheme = "onedark_dark",
             },
         },
         {

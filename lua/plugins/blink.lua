79ee436c4fb06982b2e853a1346b5ab567070a13
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-01-23 19:22:01 +0530

    small change

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,6 +7,7 @@ return {
             ["<C-j>"] = { "select_next", "fallback" },
             ["<C-u>"] = { "scroll_documentation_up", "fallback" },
             ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+            ["<c-l>"] = { "snippet_forward", "fallback" },
         },
         completion = {
             documentation = {

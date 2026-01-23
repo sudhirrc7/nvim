f22ca444009f9340ce2dedcf600f9e2c26803e10
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-01-23 10:48:42 +0530

    updated config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
             "LazyVim/LazyVim",
             import = "lazyvim.plugins",
             opts = {
-                colorscheme = "onedark_dark",
+                colorscheme = "tokyonight-moon",
             },
         },
         {
--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,4 +1,5 @@
 return {
     "olimorris/onedarkpro.nvim",
+    enabled = false,
     priority = 1000, -- Ensure it loads first
 }

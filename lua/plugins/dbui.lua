4256b2728763161182c8f872e403945e20f675f3
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-01-24 19:31:40 +0530

    clean

--- lua/plugins/dbui.lua
+++ lua/plugins/dbui.lua
@@ -1,9 +1,5 @@
 return {
     "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-        { "tpope/vim-dadbod", lazy = true },
-        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-    },
     cmd = {
         "DBUI",
         "DBUIToggle",

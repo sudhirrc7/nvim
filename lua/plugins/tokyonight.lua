32f6d40fa26f89b94ef3526bfd30da3dbb2b86e0
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-02-16 23:30:31 +0530

    basic

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,7 +3,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-        transparent = true,
+        transparent = false,
         styles = {
             sidebars = "transparent",
             floats = "transparent",

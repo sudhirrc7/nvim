71b166184f38eb0687231d554a964cc05bdc478e
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-03-03 01:06:49 +0530

    added one dark

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
             "LazyVim/LazyVim",
             import = "lazyvim.plugins",
             opts = {
-                colorscheme = "tokyonight-night",
+                colorscheme = "onedark_dark",
             },
         },
         {

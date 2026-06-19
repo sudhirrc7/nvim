aa4db7ba824daa547a4aaf6c789f009061ab0208
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-06-19 11:13:00 +0530

    19/05/2026

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,28 @@
+return {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    opts = {
+        styles = { -- For example, to apply bold and italic, use "bold,italic"
+            types = "NONE", -- Style that is applied to types
+            methods = "NONE", -- Style that is applied to methods
+            numbers = "NONE", -- Style that is applied to numbers
+            strings = "NONE", -- Style that is applied to strings
+            comments = "italic", -- Style that is applied to comments
+            keywords = "NONE", -- Style that is applied to keywords
+            constants = "NONE", -- Style that is applied to constants
+            functions = "NONE", -- Style that is applied to functions
+            operators = "NONE", -- Style that is applied to operators
+            variables = "NONE", -- Style that is applied to variables
+            parameters = "NONE", -- Style that is applied to parameters
+            conditionals = "NONE", -- Style that is applied to conditionals
+            virtual_text = "NONE", -- Style that is applied to virtual text
+        },
+        options = {
+            cursorline = true, -- Use cursorline highlighting?
+            transparency = true, -- Use a transparent background?
+            terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+            lualine_transparency = true, -- Center bar transparency?
+            highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
+        },
+    },
+}
--- lua/plugins/solarized.lua
+++ lua/plugins/solarized.lua
@@ -4,6 +4,10 @@ return {
     enabled = true,
     priority = 1000,
     opts = {
-        transparent = false, -- Enable this to disable setting the background color
+        transparent = true, -- Enable this to disable setting the background color
+        styles = {
+            sidebars = "transparent", -- Makes sidebars like NvimTree, neo-tree transparent
+            floats = "transparent", -- Optional: Makes floating windows transparent
+        },
     },
 }

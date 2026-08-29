9918e21621367fc5be663867f64ca09fa26f93d2
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-08-29 19:26:59 +0530

    removed files

--- lua/plugins/solarized.lua
+++ lua/plugins/solarized.lua
@@ -4,6 +4,11 @@ return {
     enabled = true,
     priority = 1000,
     opts = {
+        on_highlights = function(highlights, colors)
+            highlights.WinSeparator = {
+                fg = colors.blue,
+            }
+        end,
         transparent = false, -- Enable this to disable setting the background color
         styles = {
             sidebars = "transparent", -- Makes sidebars like NvimTree, neo-tree transparent

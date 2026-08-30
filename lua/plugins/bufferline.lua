d5de649b56db1e936e32f68f5f677a762cbf8669
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-08-30 13:13:02 +0530

    little changes

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,17 +1,35 @@
 return {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    enabled = false,
-    opts = {
-        options = {
-            -- mode = "tabs",
-            -- separator_style = "slant",
-            -- show_buffer_close_icons = false,
-            -- show_close_icon = false,
-            hover = {
-                enabled = true,
-                delay = 200,
-                reveal = { "close" },
+    {
+        "akinsho/bufferline.nvim",
+        event = "VeryLazy",
+        enabled = true,
+
+        opts = {
+            options = {
+                -- Your bufferline options
+            },
+        },
+
+        config = function(_, opts)
+            require("bufferline").setup(opts)
+
+            -- Hide Bufferline by default
+            vim.opt.showtabline = 0
+        end,
+
+        keys = {
+            {
+                "<leader>uA",
+                function()
+                    Snacks.toggle
+                        .option("showtabline", {
+                            off = 0,
+                            on = 2,
+                            name = "Bufferline",
+                        })
+                        :toggle()
+                end,
+                desc = "Toggle Bufferline",
             },
         },
     },
